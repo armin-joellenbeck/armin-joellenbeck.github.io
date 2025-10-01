70e6b1a30aaa9a6b99d5e84c999dc0f04230fcad
Armin Jöllenbeck <armin@joellenbeck.net>
2025-10-01 16:45:01 +0200

    fix: pin workspaces to screens

--- nixos/armin/features/i3.nix
+++ nixos/armin/features/i3.nix
@@ -38,6 +38,22 @@
         "Ctrl+Mod1+Delete" = "exec i3-msg exit";
         "Shift+Mod4+Return" = "exec google-chrome-stable";
       };
+
+      workspaceOutputAssign = [
+        { workspace = "1"; output = "HDMI-1"; }
+        { workspace = "2"; output = "HDMI-1"; }
+        { workspace = "3"; output = "HDMI-1"; }
+        { workspace = "4"; output = "HDMI-1"; }
+        { workspace = "5"; output = "HDMI-1"; }
+        { workspace = "6"; output = "HDMI-1"; }
+        { workspace = "7"; output = "HDMI-1"; }
+        { workspace = "8"; output = "HDMI-1"; }
+        { workspace = "9"; output = "VGA-1"; }
+        { workspace = "10"; output = "VGA-1"; }
+      ];
+
+      # xsession.windowManager.i3.config.workspaceOutputAssign.*.output
+      # xsession.windowManager.i3.config.workspaceOutputAssign.*.workspace
     };
   };
 
