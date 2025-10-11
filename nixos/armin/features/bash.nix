f70a81fe805c3026947ba69f040894e0eec8d4a2
Armin Jöllenbeck <armin@joellenbeck.net>
2025-10-11 20:12:12 +0200

    feat(nixos): aliases for ls and tree

--- nixos/armin/features/bash.nix
+++ nixos/armin/features/bash.nix
@@ -5,8 +5,12 @@
     enable = true;
 
     shellAliases = {
-      ll = "ls -al";
       ls = "ls --color=auto --group-directories-first -v";
+      l = "ls -A";
+      ll = "l -l";
+      tree = "tree --dirsfirst -v";
+      tt = "tree -a";
+      t = "tt -I .git --gitignore";
     };
   };
 }
